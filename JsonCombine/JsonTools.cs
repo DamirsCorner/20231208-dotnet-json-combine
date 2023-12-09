c2e6f3bdacfa6c3e0e796b6399eab43f97a9724d
Damir Arh <damir.arh@gmail.com>
2023-12-09 10:09:30 +0100

    Use JSON DOM directly

--- JsonCombine/JsonTools.cs
+++ JsonCombine/JsonTools.cs
@@ -1,10 +1,10 @@
-﻿using System.Text.Json;
+﻿using System.Text.Json.Nodes;
 
 namespace JsonCombine;
 public static class JsonTools
 {
     public static string ToArray(params string[] jsons)
     {
-        return JsonSerializer.Serialize(jsons.Select(json => JsonSerializer.Deserialize<JsonElement>(json)));
+        return new JsonArray(jsons.Select(json => JsonNode.Parse(json)).ToArray()).ToJsonString();
     }
 }
